92f2fedb3600a6bbf4d2e4fb54053a216aa7f940
gaoyx <melo@gaoyuexiang.cn>
2017-07-24 16:35:09 +0800

    finish case when given 30

--- src/main/java/cn/yxgao/twu/prime/factor/exercise/Generator.java
+++ src/main/java/cn/yxgao/twu/prime/factor/exercise/Generator.java
@@ -1,12 +1,28 @@
 package cn.yxgao.twu.prime.factor.exercise;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
 
 public class Generator {
   public List<Integer> generate(int n) {
-    return emptyList();
+    if (n == 1) {
+      return emptyList();
+    }
+    ArrayList<Integer> factors = new ArrayList<>();
+    for (int i = 2; i <= n; i++) {
+      while(n >= i) {
+        if (n % i != 0) {
+          break;
+        }
+        factors.add(i);
+        n = n / i;
+      }
+    }
+    return factors.stream().sorted().collect(toList());
   }
 }
--- src/test/java/cn/yxgao/twu/prime/factor/exercise/GeneratorTest.java
+++ src/test/java/cn/yxgao/twu/prime/factor/exercise/GeneratorTest.java
@@ -3,6 +3,7 @@ package cn.yxgao.twu.prime.factor.exercise;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
@@ -21,5 +22,12 @@ public class GeneratorTest {
   public void should_return_empty_list_when_given_1() throws Exception {
     List<Integer> factors = generator.generate(1);
     assertEquals(factors, emptyList());
+  }
+
+  @Test
+  public void shuld_return_expect_list_when_given_30() throws Exception {
+    List<Integer> expectFactors = Arrays.asList(2, 3, 5);
+    List<Integer> factors = generator.generate(30);
+    assertEquals(expectFactors, factors);
   }
 }
